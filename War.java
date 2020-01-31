7cae9fd7ce079b52f986db1850c3144e96a0c659
stfnylim <31866447+stfnylim@users.noreply.github.com>
2020-01-30 20:52:37 -0800

    Add files via upload

--- War.java
+++ War.java
@@ -3,7 +3,11 @@ package War;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-
+/**
+ * 
+ * @author stfnylim
+ *
+ */
 
 public class War {
 	public ArrayList<Player> players = new ArrayList<Player>();
@@ -67,27 +71,76 @@ public class War {
 		**/
 		
 	}
+	public void empty() {
+		
+	}
+	
 	
 	public void shuffle() {
 		deck.shuffle();
 	}
 	
-	public void play() {
-		
+	public void playCard() {	
 		for (int i = 0; i<numPlayers; i++) {
 			System.out.println(players.get(i)+" played a " + players.get(i).hand.get(0));
 			diPile.add(players.get(i).hand.get(0));
 			players.get(i).hand.remove(0);
-
 		}
+		
+		
+	}
+	
+	public void playWar() {
+		for (int i = 0; i<numPlayers; i++) {
+			for (int j = 0; j <3; j++) {
+				System.out.println(players.get(i)+" played a random card");
+				diPile.add(players.get(i).hand.get(0));
+				players.get(i).hand.remove(0);
+			}
+			
+			
+			
+		}
+		this.playCard();
+	}
+	
+	public void playAllWar() {
+		
+		this.playCard();
+		
+		//while (players.size()>1) {
+			// to compare the cards in the discard pile.
+			if (diPile.get(0).compareTo(diPile.get(1))<0) {
+				for (int i =0; i <diPile.size();i++) {
+					players.get(1).hand.add(diPile.get(i));
+					System.out.println(players.get(1) + " wins the round");
+				}
+			}
+			else if(diPile.get(0).compareTo(diPile.get(1))>0) {
+				for (int i =0; i <diPile.size();i++) {
+					players.get(0).hand.add(diPile.get(i));
+					System.out.println(players.get(0) + " wins the round");
+				}
 
-		
-		
-		//if () {}
+			}
+			
+			else {
+				//war game start if the cards are equal
+				System.out.println("War game has started.");
+				this.playWar();
+				
+			}
+			
+			
+			
+		//}
+			
 		
 		
 		
 	}
+	
+	
 	
 	public String toString() {
 		return "There are " + numPlayers + " players playing this game of War.";
